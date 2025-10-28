62ed4305b5e2d9e6a5caa1d9a694561c9f0ec10c
Nathaniel Sandalow-Ash <sandalowash@gmail.com>
2025-10-27 20:32:47 -0400

    no 2

--- src/tricks.js
+++ src/tricks.js
@@ -2,5 +2,6 @@ export const TRICKS = {
   anagram:   { label: "Anagram",   blurb: "Anagram of answer and hint that there’s an anagram; ‘Senator arranged crime (7)’ gives TREASON" },
   charade: { label: "Charade", blurb: "Combination of synonyms/abbrevs; ‘Qualify to get drink for ID (8)’ gives PASSPORT (pass + port)" },
   deletion: { label: "Deletion", blurb: "Remove letters from another word for answer; ‘Car trip with no parking, bad thing (3)’ gives SIN" },
-  double_definition: { label: "Double Definition", blurb: "Both halves are definitions; ‘Search scrub (5)’ gives SCOUR"}
+  double_definition: { label: "Double Definition", blurb: "Both halves are definitions; ‘Search scrub (5)’ gives SCOUR"},
+  acrostic: { label: "Acrostic", blurb: "First letters of answer; ‘Initially get a good joke (3)’ gives GAG"}
 };
